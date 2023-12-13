82b3c5c286a83e19c4c47052ce49ca6d6e61bbf8
kamisc <kamil.seweryn@o2.pl>
2023-12-13 22:30:10 +0100

    initial commit

---
+++ src/main/java/com/sewerynkamil/elasticsearch/ElasticsearchDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sewerynkamil.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchDemoProjectApplication.class, args);
+	}
+
+}
